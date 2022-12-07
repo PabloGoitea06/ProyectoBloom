7c5e4f79a06cfaa2e84c7f7706b0cbefdbdd68fe
Pablo Goitea <pablogoitea@hotmail.com>
2022-12-06 23:56:01 -0300

    1

--- js/script.js
+++ js/script.js
@@ -53,6 +53,7 @@ const elementosToggleables = document.querySelectorAll(".toggeable");
 
 const btnLogout = document.getElementById("logout");
 
+
 function validarUsuario(usersDB, mail, pass) {
   let encontrado = usersDB.find((userDB) => userDB.mail == mail);
   console.log(encontrado);
@@ -110,6 +111,7 @@ function presentarInfo(array, clase) {
   });
 }
 
+
 //Esta función revisa si hay un usuario guardado en el storage, y en ese caso evita todo el proceso de login
 function estaLogueado(usuario) {
   if (usuario) {
@@ -118,17 +120,14 @@ function estaLogueado(usuario) {
   }
 }
 
+
 btnLogin.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (!inputMailLogin.value || !inputPassLogin.value) {
     alert("Todos los campos son requeridos");
   } else {
-    let data = validarUsuario(
-      usuarios,
-      inputMailLogin.value,
-      inputPassLogin.value
-    );
+    let data = validarUsuario(usuarios,inputMailLogin.value,inputPassLogin.value);
     if (!data) {
       alert("Usuario y/o contraseña erróneos");
     } else {
@@ -156,13 +155,21 @@ window.onload = () => {
   estaLogueado(recuperarUsuario(localStorage));
 };
 
+
+
+
+
 const nombreYApellido = document.getElementById("name");
-const emailF= document.getElementById("email");
-const resultF = document.getElementById("areadetexto");
-const conformidad = getElementById("selector");
+const emailFormu = document.getElementById("email");
+const resultFormu = document.getElementById("areadetexto");
+const conformidad = document.getElementById("selector");
 const btnLogueo = document.getElementById("btnEnviar");
 
-/*
+
+
+
+
+/* 
 para cuando el carrito esta vacio podemos usar :
 
 const carrito: [];
@@ -172,6 +179,11 @@ carrito.length === 0 && alert ('carrito vacío');
 */
 
 
+
+
+
+
+
 document.getElementById("btnEnviar").onsubmit = function() {
   var email = document.getElementById("name").value
   var subject = document.getElementById("email").value
@@ -179,5 +191,3 @@ document.getElementById("btnEnviar").onsubmit = function() {
 
   window.open('mailto:'+ email +'?subject='+ subject +'&body='+body );
 }
-
-
